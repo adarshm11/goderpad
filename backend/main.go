a22d7a123fca617d5aceb74b8fff6327c091cef1
jask1m <jkim94960@gmail.com>
2025-12-03 10:27:49 -0800

    Clerk user creation webhook + dummy endpoint

--- backend/main.go
+++ backend/main.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"fmt"
-
+	"log"
 	"net/http"
+	"os"
+
+	_ "github.com/joho/godotenv/autoload"
 
 	"github.com/gin-gonic/gin"
 	"github.com/gorilla/websocket"
+	svix "github.com/svix/svix-webhooks/go"
 )
 
 var upgrader = websocket.Upgrader{
@@ -18,6 +22,11 @@ var upgrader = websocket.Upgrader{
 }
 
 func main() {
+	webhookSecret := os.Getenv("CLERK_WEBHOOK_SIGNING_SECRET")
+	if webhookSecret == "" {
+		log.Fatal("CLERK_WEBHOOK_SIGNING_SECRET is not set")
+	}
+
 	router := gin.Default()
 
 	router.GET("/ws", func(c *gin.Context) {
@@ -44,6 +53,28 @@ func main() {
 				break
 			}
 		}
+	})
+
+	// Clerk webhook endpoint for `user.created` event
+	// TODO: write user metadata to MongoDB
+	// TODO: setup another webhook for `user.updated` event
+	router.POST("/api/webhooks", func(c *gin.Context) {
+		body, _ := c.GetRawData()
+
+		headers := http.Header{}
+		headers.Set("svix-id", c.GetHeader("svix-id"))
+		headers.Set("svix-timestamp", c.GetHeader("svix-timestamp"))
+		headers.Set("svix-signature", c.GetHeader("svix-signature"))
+
+		webhook, _ := svix.NewWebhook(webhookSecret)
+		if err := webhook.Verify(body, headers); err != nil {
+			log.Printf("webhook: verification failed: %v", err)
+			c.Status(http.StatusBadRequest)
+			return
+		}
+
+		log.Printf("webhook: received payload: %s", string(body))
+		c.Status(http.StatusOK)
 	})
 
 	router.Run(":8080")
