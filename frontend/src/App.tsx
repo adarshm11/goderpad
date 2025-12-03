401ea4931fee3f6a790f55c281d15423c447296f
jask1m <jkim94960@gmail.com>
2025-12-02 22:39:29 -0800

    Install Clerk for auth

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,27 @@
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from '@clerk/clerk-react'
 import WebSocketTestClient from './WebSocketTestClient.tsx'
 
 export default function App() {
   return (
-    <WebSocketTestClient />
-  );
+    <>
+      <header>
+        <SignedOut>
+          <SignInButton />
+          <SignUpButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </header>
+      <main>
+        <WebSocketTestClient />
+      </main>
+    </>
+  )
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,14 +2,23 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BrowserRouter, Routes, Route } from 'react-router'
+import { ClerkProvider } from '@clerk/clerk-react'
 import App from './App.tsx'
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Missing Clerk Publishable Key')
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-      </Routes>
-    </BrowserRouter>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+        </Routes>
+      </BrowserRouter>
+    </ClerkProvider>
   </StrictMode>,
 )
